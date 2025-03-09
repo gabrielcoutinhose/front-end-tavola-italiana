af88ee02c43b08d3a519ea4a20f5fe139b2ccef2
gabrielcoutinhose <gabrielctnhse@gmail.com>
2025-03-09 18:29:38 -0300

    feat: adding global style and styled components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Login from './containers/Login';
+import GlobalStyles from './styles/globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
+  <>
+    <GlobalStyles />
     <h1>Tavola Italiana</h1>
     <Login />
-  </div>
+  </>
 );

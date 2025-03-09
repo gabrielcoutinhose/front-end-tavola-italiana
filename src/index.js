2dd12b0a37a724399649550f7e3085b7d5861cbf
gabrielcoutinhose <gabrielctnhse@gmail.com>
2025-03-08 22:13:29 -0300

    refactor:  package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Login from './containers/Login';
+import Login from   './containers/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+return   <div>
   <h1>Tavola Italiana</h1>
   <Login />
-  </>
+</div>;
 );
